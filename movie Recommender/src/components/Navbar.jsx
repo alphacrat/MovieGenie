56ab8c7122e185bf25cea32b6338c0c4972856e6
alphacrat <bismoydas333@gmail.com>
2025-02-02 14:44:29 +0530

    changes made in dist part 2

--- movie Recommender/src/components/Navbar.jsx
+++ movie Recommender/src/components/Navbar.jsx
@@ -90,8 +90,8 @@ export default function Navbar() {
                             {["Home", "About", "Favorites"].map((item, index) => (
                                 <li key={index}>
                                     <a
-                                        href={item.toLowerCase() === 'home' ? '/' : `/${item.toLowerCase()}`}
-                                        className={`block py-2 px-3 rounded-sm ${window.location.pathname === (item.toLowerCase() === 'home' ? '/' : `/${item.toLowerCase()}`)
+                                        href={item.toLowerCase() === 'home' ? '/' : `#/${item.toLowerCase()}`}
+                                        className={`block py-2 px-3 rounded-sm ${window.location.pathname === (item.toLowerCase() === 'home' ? '/' : `#/${item.toLowerCase()}`)
                                             ? "text-white bg-blue-700 md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
                                             : "text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                                             }`}
